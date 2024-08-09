8d119defe6bbb8a5b1b7030221932585d15924d0
DoHwan <jx7789@gmail.com>
2024-08-09 18:32:59 +0900

    feat.react-router-dom

---
+++ src/pages/todo/IndexPage.js
@@ -0,0 +1,32 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import BasicLayout from "../../layouts/BasicLayout";
+import { useCallback } from "react";
+
+const IndexPage = () => {
+  const navigate = useNavigate();
+  const handleClickList = useCallback(() => {
+    navigate({ pathname: "list" });
+  });
+  const handleClickAdd = useCallback(() => {
+    navigate({ pathname: "add" });
+  });
+  return (
+    <BasicLayout>
+      <div className="w-full flex flex-col m-2 p-2">
+        <div className="flex">
+          <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline" onClick={handleClickList}>
+            LIST
+          </div>
+          <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline" onClick={handleClickAdd}>
+            ADD
+          </div>
+        </div>
+        <div className="w-full mt-4">
+          <Outlet />
+        </div>
+      </div>
+    </BasicLayout>
+  );
+};
+
+export default IndexPage;
