f42e2b952040a419f37ed3298c49def13d4eebda
DoHwan <jx7789@gmail.com>
2024-08-14 20:07:35 +0900

    feat.product 관련 React-Router 설정

---
+++ src/router/productsRouter.js
@@ -0,0 +1,23 @@
+import { lazy, Suspense } from "react";
+import { Navigate } from "react-router-dom";
+
+const productsRouter = () => {
+  const Loading = <div>Loading....</div>;
+  const ProductsList = lazy(() => import("../pages/products/ListPage"));
+  return [
+    {
+      path: "list",
+      element: (
+        <Suspense fallback={Loading}>
+          <ProductsList />
+        </Suspense>
+      ),
+    },
+    {
+      path: "",
+      element: <Navigate replace to="/products/list/" />,
+    },
+  ];
+};
+
+export default productsRouter;
