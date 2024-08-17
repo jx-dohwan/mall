9da9070f1c3281128e946ba98d433c5be518949c
DoHwan <jx7789@gmail.com>
2024-08-17 18:36:04 +0900

    feat.조회 페이지와 조회 컴포넌트 & 수정/삭제 페이지와 컴포넌트 처리

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -5,6 +5,8 @@ const productsRouter = () => {
   const Loading = <div>Loading....</div>;
   const ProductsList = lazy(() => import("../pages/products/ListPage"));
   const ProductsAdd = lazy(() => import("../pages/products/AddPage"));
+  const ProductRead = lazy(() => import("../pages/products/ReadPage"));
+  const ProductModify = lazy(() => import("../pages/products/ModifyPage"));
 
   return [
     {
@@ -24,6 +26,22 @@ const productsRouter = () => {
       element: (
         <Suspense fallback={Loading}>
           <ProductsAdd />
+        </Suspense>
+      ),
+    },
+    {
+      path: "read/:pno",
+      element: (
+        <Suspense fallback={Loading}>
+          <ProductRead />
+        </Suspense>
+      ),
+    },
+    {
+      path: "modify/:pno",
+      element: (
+        <Suspense fallback={Loading}>
+          <ProductModify />
         </Suspense>
       ),
     },
